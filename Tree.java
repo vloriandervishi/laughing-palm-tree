4efaed96a649a377d8e4463dd0a124e0472f2aab
Valdo <vlorian22@GMAIL.COM>
2021-06-06 12:25:32 -0500

    Plant A tree Java Project

---
+++ Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args){
+      // We're going to introduce ourselves and print a tree to the scree.
+      System.out.println("Hey there, I'm Vlorian and I'm learning to code in Java!");
+      System.out.println("Hey there, I'm going to plant a tree today! or "Ready to get my hands dirty"");
+     System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  ");
+    }
+  }
